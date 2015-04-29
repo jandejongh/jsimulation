8f7fc87fff28bd4ddf0b20e87efcb120d2ae44ee
Jan de Jongh <jfcmdejongh@gmail.com>
2015-04-29 19:16:39 +0000

    Bug fix in handling events without associated action (did not update event-list time). Lots of javadoc.
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jsimulation/trunk@177 462c2269-1816-4c7e-b5c6-2c6da641f8cc

--- java/nl/jdj/jsimulation/r2/SimEventList.java
+++ java/nl/jdj/jsimulation/r2/SimEventList.java
@@ -7,9 +7,14 @@ import java.util.TreeSet;
 
 /** An event list for {@link SimEvent}s, implemented as a {@link TreeSet}.
  * 
+ * Since a {@link TreeSet} is being used for bookkeeping of the events, the
+ * events themselves must be totally ordered.
+ * 
  * The current implementation is not thread-safe.
  * 
  * @param <E> The type of {@link SimEvent}s supported.
+ * 
+ * @see DefaultSimEventComparator
  * 
  */
 public class SimEventList<E extends SimEvent>
@@ -22,7 +27,7 @@ public class SimEventList<E extends SimEvent>
   
   /** Returns the current time during processing of the event list.
    * 
-   * @return The current time, zero or positive.
+   * @return The current time.
    * 
    */
   public final double getTime ()
@@ -40,6 +45,16 @@ public class SimEventList<E extends SimEvent>
    */
   private final Set<SimEventAction> updateListeners = new HashSet<> ();
   
+  /** Adds a listener for updates to the current time in this event list.
+   * 
+   * Beware that listeners are only invoked upon changes in the time; which may the result of
+   * processing multiple events in sequence.
+   * 
+   * @param a The action to be added.
+   * 
+   * @throws IllegalArgumentException If <code>a == null</code>.
+   * 
+   */
   public final void addUpdateSimEventAction (SimEventAction a)
   {
     if (a == null)
@@ -47,6 +62,13 @@ public class SimEventList<E extends SimEvent>
     this.updateListeners.add (a);
   }
   
+  /** Removes a listener for updates to the current time in this event list.
+   * 
+   * @param a The action to be removed (ignored if not present).
+   * 
+   * @throws IllegalArgumentException If <code>a == null</code>.
+   * 
+   */
   public final void removeUpdateSimEventAction (SimEventAction a)
   {
     if (a == null)
@@ -54,16 +76,39 @@ public class SimEventList<E extends SimEvent>
     this.updateListeners.remove (a);
   }
   
+  /** Creates a new {@link SimEventList} with default {@link Comparator}.
+   * 
+   * @see DefaultSimEventComparator
+   * 
+   */
   public SimEventList ()
   {
     this (new DefaultSimEventComparator ());
   }
 
+  /** Creates a new {@link SimEventList} with given {@link Comparator}.
+   * 
+   * @param comparator The comparator for {@link SimEvent}s.
+   * 
+   */
   public SimEventList (Comparator comparator)
   {
     super (comparator);
   }
 
+  /** Checks the progress of time when processing a given event
+   * and triggers actions on update listeners when an update has taken place.
+   * 
+   * An update is defined as the processing of the first event or an increase in the current time.
+   * If needed, this method updates the current time.
+   * 
+   * @param e The event being processed.
+   * 
+   * @see #getTime
+   * @see #addUpdateSimEventAction
+   * @see SimEventAction
+   * 
+   */ 
   public void checkUpdate (E e)
   {
     if (this.firstUpdate || e.getTime () > this.lastUpdateTime)
@@ -92,12 +137,10 @@ public class SimEventList<E extends SimEvent>
     while (! isEmpty ())
     {
       final E e = pollFirst ();
+      checkUpdate (e);
       final SimEventAction a = e.getEventAction ();
       if (a != null)
-      {
-        checkUpdate (e);
         a.action (e);
-      }
     }
   }
   
