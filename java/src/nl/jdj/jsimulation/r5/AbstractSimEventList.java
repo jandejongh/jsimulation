ce000ea955b99913dfdf64ff90b72fd40cfab293
Jan de Jongh <jfcmdejongh@gmail.com>
2017-07-03 10:57:19 +0000

    Using arrays for event-list listeners in order to circumvent massive Iterator generation. Minor javadoc fixes. [PRESUMED CLEAN COMMIT]
    git-svn-id: https://jandejongh.dnsdojo.org:3690/jsimulation/trunk@245 462c2269-1816-4c7e-b5c6-2c6da641f8cc

--- java/src/nl/jdj/jsimulation/r5/AbstractSimEventList.java
+++ java/src/nl/jdj/jsimulation/r5/AbstractSimEventList.java
@@ -24,6 +24,18 @@ extends TreeSet<E>
 implements SimEventList<E>
 {
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // USE ARRAY OPTIMIZATION [COMPILE-TIME SWITCH]
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** When {@code true}, this class and possibly some sub-classes maintain various array copies of collections
+   *  often iterated over, like listeners, and uses array iteration instead of Collection iteration.
+   * 
+   */
+  protected final static boolean USE_ARRAY_OPTIMIZATION = true;
+  
   /** Creates a new {@link SimEventList} for plain {@link SimEvent}s with default {@link Comparator}.
    * 
    * <p>
@@ -84,6 +96,10 @@ implements SimEventList<E>
    * <p>
    * You do not have to override this method in case you want to change
    * setting the de-conflict value; override {@link #add} instead.
+   * 
+   * @param collection See {@link TreeSet#addAll}.
+   * 
+   * @return See {@link TreeSet#addAll}.
    * 
    * @see SimEvent#setSimEventListDeconflictValue
    * 
@@ -169,22 +185,48 @@ implements SimEventList<E>
    */
   private final Set<SimEventListListener> listeners = new HashSet<> ();
   
+  /** The listeners in array form, if {@code USE_ARRAY_OPTIMIZATION == true}.
+   * 
+   */
+  private SimEventListListener[] listenersAsArray = new SimEventListListener[0];
+
   /** The listeners to this event list that need per-event notifications.
    * 
    */
   private final Set<SimEventListListener.Fine> fineListeners = new HashSet<> ();
   
+  /** The fine listeners in array form, if {@code USE_ARRAY_OPTIMIZATION == true}.
+   * 
+   */
+  private SimEventListListener.Fine[] fineListenersAsArray = new SimEventListListener.Fine[0];
+
   @Override
   public final void addListener (SimEventListResetListener l)
   {
     if (l != null)
     {
       if (l instanceof SimEventListListener.Fine)
-        this.fineListeners.add ((SimEventListListener.Fine) l);
+      {
+        if (! this.fineListeners.contains ((SimEventListListener.Fine) l))
+        {
+          this.fineListeners.add ((SimEventListListener.Fine) l);
+          if (AbstractSimEventList.USE_ARRAY_OPTIMIZATION)
+            this.fineListenersAsArray = this.fineListeners.toArray (new SimEventListListener.Fine[this.fineListeners.size ()]);
+        }
+      }
       else if (l instanceof SimEventListListener)
-        this.listeners.add ((SimEventListListener) l);
+      {
+        if (! this.listeners.contains ((SimEventListListener) l))
+        {
+          this.listeners.add ((SimEventListListener) l);
+          if (AbstractSimEventList.USE_ARRAY_OPTIMIZATION)
+            this.listenersAsArray = this.listeners.toArray (new SimEventListListener[this.listeners.size ()]);
+        }
+      }
       else
+      {
         this.resetListeners.add (l);
+      }
     }
   }
 
@@ -195,9 +237,23 @@ implements SimEventList<E>
     {
       this.resetListeners.remove (l);
       if (l instanceof SimEventListListener)
-        this.listeners.remove ((SimEventListListener) l);
+      {
+        if (this.listeners.contains ((SimEventListListener) l))
+        {
+          this.listeners.remove ((SimEventListListener) l);
+          if (AbstractSimEventList.USE_ARRAY_OPTIMIZATION)
+            this.listenersAsArray = this.listeners.toArray (new SimEventListListener[this.listeners.size ()]);
+        }
+      }
       if (l instanceof SimEventListListener.Fine)
-        this.fineListeners.remove ((SimEventListListener.Fine) l);
+      {
+        if (this.fineListeners.contains ((SimEventListListener.Fine) l))
+        {
+          this.fineListeners.remove ((SimEventListListener.Fine) l);
+          if (AbstractSimEventList.USE_ARRAY_OPTIMIZATION)
+            this.fineListenersAsArray = this.fineListeners.toArray (new SimEventListListener.Fine[this.fineListeners.size ()]);
+        }
+      }
     }
   }
 
@@ -223,10 +279,24 @@ implements SimEventList<E>
    */
   protected final void fireUpdate ()
   {
-    for (SimEventListListener.Fine l : this.fineListeners)
-      l.notifyEventListUpdate (this, this.lastUpdateTime);    
-    for (SimEventListListener l : this.listeners)
-      l.notifyEventListUpdate (this, this.lastUpdateTime);    
+    if (AbstractSimEventList.USE_ARRAY_OPTIMIZATION)
+    {
+      if (! this.fineListeners.isEmpty ())
+        for (SimEventListListener.Fine l : this.fineListenersAsArray)
+          l.notifyEventListUpdate (this, this.lastUpdateTime);
+      if (! this.listeners.isEmpty ())
+        for (SimEventListListener l : this.listenersAsArray)
+          l.notifyEventListUpdate (this, this.lastUpdateTime);      
+    }
+    else
+    {
+      if (! this.fineListeners.isEmpty ())
+        for (SimEventListListener.Fine l : this.fineListeners)
+          l.notifyEventListUpdate (this, this.lastUpdateTime);
+      if (! this.listeners.isEmpty ())
+        for (SimEventListListener l : this.listeners)
+          l.notifyEventListUpdate (this, this.lastUpdateTime);
+    }
   }
   
   /** Fires an empty-event-list notification to registered listeners.
@@ -236,10 +306,24 @@ implements SimEventList<E>
    */
   protected final void fireEmpty ()
   {
-    for (SimEventListListener.Fine l : this.fineListeners)
-      l.notifyEventListEmpty (this, this.lastUpdateTime);    
-    for (SimEventListListener l : this.listeners)
-      l.notifyEventListEmpty (this, this.lastUpdateTime);    
+    if (AbstractSimEventList.USE_ARRAY_OPTIMIZATION)
+    {
+      if (! this.fineListeners.isEmpty ())
+        for (SimEventListListener.Fine l : this.fineListenersAsArray)
+          l.notifyEventListEmpty (this, this.lastUpdateTime);
+      if (! this.listeners.isEmpty ())
+        for (SimEventListListener l : this.listenersAsArray)
+          l.notifyEventListEmpty (this, this.lastUpdateTime);      
+    }
+    else
+    {
+      if (! this.fineListeners.isEmpty ())
+        for (SimEventListListener.Fine l : this.fineListeners)
+          l.notifyEventListEmpty (this, this.lastUpdateTime);
+      if (! this.listeners.isEmpty ())
+        for (SimEventListListener l : this.listeners)
+          l.notifyEventListEmpty (this, this.lastUpdateTime);
+    }
   }
   
   /** Fires a next-event notification to registered listeners.
@@ -249,8 +333,18 @@ implements SimEventList<E>
    */
   protected final void fireNextEvent ()
   {
-    for (SimEventListListener.Fine l : this.fineListeners)
-      l.notifyNextEvent (this, this.lastUpdateTime);
+    if (AbstractSimEventList.USE_ARRAY_OPTIMIZATION)
+    {
+      if (! this.fineListeners.isEmpty ())
+        for (SimEventListListener.Fine l : this.fineListenersAsArray)
+          l.notifyNextEvent (this, this.lastUpdateTime);
+    }
+    else
+    {
+      if (! this.fineListeners.isEmpty ())
+        for (SimEventListListener.Fine l : this.fineListeners)
+          l.notifyNextEvent (this, this.lastUpdateTime);
+    }
   }
   
   /** Checks the progress of time and notifies listeners when an update has taken place.
