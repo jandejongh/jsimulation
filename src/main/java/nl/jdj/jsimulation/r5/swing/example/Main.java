1b8f9d78a921d8ae179dd3d6433dbde202e0d54b
jan <jan@rush.jdj>
2018-04-04 20:56:02 +0200

    r5.1.0 release.

--- src/main/java/nl/jdj/jsimulation/r5/swing/example/Main.java
+++ src/main/java/nl/jdj/jsimulation/r5/swing/example/Main.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010-2018 Jan de Jongh, TNO.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package nl.jdj.jsimulation.r5.swing.example;
 
 import java.awt.Color;
@@ -21,6 +37,9 @@ import nl.jdj.jsimulation.r5.swing.JSimEventList;
 
 /** Example program showing the use of {@link JSimEventList} and basic interactions with a {@link SimEventList}.
  *
+ * <p>
+ * <b>Last javadoc Review:</b> Jan de Jongh, TNO, 20180404, r5.1.0.
+ * 
  * @see SimEventList
  * @see JSimEventList
  *
@@ -37,7 +56,7 @@ public final class Main
   
   /** Starts the main program.
    * 
-   * @param args The command line arguments.
+   * @param args The command line arguments (ignored).
    * 
    */
   public static void main (final String[] args)
@@ -196,5 +215,11 @@ public final class Main
         }
       });
   }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
 }
