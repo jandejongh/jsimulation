89452d9cc463e97570d8cf9fc973f717e902bd02
jan <jan@rush.jdj>
2018-04-04 20:01:09 +0200

    r5.1.0 release.

--- src/main/java/nl/jdj/jsimulation/r5/DefaultSimEventList_IOEL.java
+++ src/main/java/nl/jdj/jsimulation/r5/DefaultSimEventList_IOEL.java
@@ -1,4 +1,22 @@
+/*
+ * Copyright 2010-2018 Jan de Jongh, TNO.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package nl.jdj.jsimulation.r5;
+
+import java.util.Set;
 
 /** The IOEL (Insertion-Order Event List) variant of {@link AbstractSimEventList}.
  * 
@@ -9,6 +27,9 @@ package nl.jdj.jsimulation.r5;
  * The implementation is not thread-safe.
  * An event list is really meant to be processed and operated upon by a single thread only.
  * 
+ * <p>
+ * <b>Last javadoc Review:</b> Jan de Jongh, TNO, 20180404, r5.1.0.
+ * 
  * @param <E> The type of {@link SimEvent}s supported.
  * 
  */
@@ -16,27 +37,80 @@ public final class DefaultSimEventList_IOEL<E extends SimEvent>
 extends AbstractSimEventList<E>
 {
 
-  /** Creates a new {@link DefaultSimEventList_IOEL} for {@link DefaultSimEvent}s.
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SERIALIZATION
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final long serialVersionUID = 7744885373343754672L;
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTORS / CLONING / FACTORIES
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Creates a new {@link DefaultSimEventList_IOEL} (main constructor).
+   * 
+   * @param defaultResetTime The default reset time.
+   * @param eventClass       The base class {@link SimEvent}s supported.
    * 
    */
-  public DefaultSimEventList_IOEL ()
+  public DefaultSimEventList_IOEL (final double defaultResetTime, final Class<E> eventClass)
   {
-    this ((Class<E>) DefaultSimEvent.class);
+    super (new DefaultSimEventComparator (), defaultResetTime, eventClass);
+    this.deconflict_IOEL = Long.MIN_VALUE;
+  }
+  
+  /** Creates a new {@link DefaultSimEventList_IOEL} for {@link DefaultSimEvent}s.
+   * 
+   * @param defaultResetTime The default reset time.
+   * 
+   */
+  public DefaultSimEventList_IOEL (final double defaultResetTime)
+  {
+    this (defaultResetTime, (Class<E>) DefaultSimEvent.class);
   }
   
   /** Creates a new {@link DefaultSimEventList_IOEL}.
    * 
    * @param eventClass The base class {@link SimEvent}s supported.
    * 
+   * <p>
+   * The default reset time is set to {@link Double#NEGATIVE_INFINITY}.
+   * 
    */
   public DefaultSimEventList_IOEL (final Class<E> eventClass)
   {
-    super (new DefaultSimEventComparator (), eventClass);
-    this.deconflict_IOEL = Long.MIN_VALUE;
+    this (Double.NEGATIVE_INFINITY, eventClass);
+  }
+
+  /** Creates a new {@link DefaultSimEventList_IOEL} for {@link DefaultSimEvent}s.
+   * 
+   * <p>
+   * The default reset time is set to {@link Double#NEGATIVE_INFINITY}.
+   * 
+   */
+  public DefaultSimEventList_IOEL ()
+  {
+    this (Double.NEGATIVE_INFINITY, (Class<E>) DefaultSimEvent.class);
   }
   
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // DECONFLICT
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
   private long deconflict_IOEL = Long.MIN_VALUE;
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // Set / TreeSet
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
   /** Overridden in order to set the <i>deconflict</i> value on the event added.
    * 
    * <p>
@@ -47,6 +121,10 @@ extends AbstractSimEventList<E>
    * simultaneous events are processed in insertion-order.
    * Note that the counter is reset in this method
    * when the list is empty upon entry.
+   * 
+   * @param e See {@link Set#add}.
+   * 
+   * @return See {@link Set#add}.
    * 
    * @see SimEvent#setSimEventListDeconflictValue
    * 
@@ -67,4 +145,10 @@ extends AbstractSimEventList<E>
     return false;
   }
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
 }
