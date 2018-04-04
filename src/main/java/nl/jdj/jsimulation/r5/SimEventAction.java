d66cebe5aaf7f03948f82e973bcb572c2dee4428
jan <jan@rush.jdj>
2018-04-04 20:21:23 +0200

    r5.1.0 release.

--- src/main/java/nl/jdj/jsimulation/r5/SimEventAction.java
+++ src/main/java/nl/jdj/jsimulation/r5/SimEventAction.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright 2010-2018 Jan de Jongh, TNO.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package nl.jdj.jsimulation.r5;
 
 /** An action provider for {@link SimEvent}s.
  * 
  * In the interface main method, {@link #action}, a {@link SimEvent} is passed.
  * As a result, a single {@link SimEventAction} interface can be used for multiple {@link SimEvent}s.
+ * 
+ * <p>
+ * <b>Last javadoc Review:</b> Jan de Jongh, TNO, 20180404, r5.1.0.
  * 
  * @param <T> The type of the user object of the {@link SimEvent}.
  * 
@@ -12,6 +31,12 @@ package nl.jdj.jsimulation.r5;
 public interface SimEventAction<T>
 {
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // ACTION
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
   /** Invokes the action for supplied {@link SimEvent}.
    *
    * @param event The event.
@@ -21,4 +46,10 @@ public interface SimEventAction<T>
    */
   public void action (SimEvent<T> event);
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
 }
