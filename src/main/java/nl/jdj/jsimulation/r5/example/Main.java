7f58982aaa883bf4e2e042dfb3f0978127f294e0
jan <jan@rush.jdj>
2018-04-04 20:36:06 +0200

    r5.1.0 release.

--- src/main/java/nl/jdj/jsimulation/r5/example/Main.java
+++ src/main/java/nl/jdj/jsimulation/r5/example/Main.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010-2018 Jan de Jongh, TNO.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package nl.jdj.jsimulation.r5.example;
 
 import nl.jdj.jsimulation.r5.AbstractSimTimer;
@@ -7,7 +23,10 @@ import nl.jdj.jsimulation.r5.SimEvent;
 import nl.jdj.jsimulation.r5.SimEventAction;
 import nl.jdj.jsimulation.r5.SimEventList;
 
-/** Example code for jsimulation.
+/** Example code for {@code jsimulation}.
+ * 
+ * <p>
+ * <b>Last javadoc Review:</b> Jan de Jongh, TNO, 20180404, r5.1.0.
  * 
  */
 public final class Main
@@ -97,5 +116,11 @@ public final class Main
     el.run ();
     System.out.println ("=== FINISHED ===");
   }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
 }
