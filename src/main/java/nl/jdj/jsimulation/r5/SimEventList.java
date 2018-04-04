187d2d81c585ffa361b84f6a3a3acb09e041972b
jan <jan@rush.jdj>
2018-04-04 20:00:13 +0200

    r5.1.0 release.

--- src/main/java/nl/jdj/jsimulation/r5/SimEventList.java
+++ src/main/java/nl/jdj/jsimulation/r5/SimEventList.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010-2018 Jan de Jongh, TNO.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package nl.jdj.jsimulation.r5;
 
 import java.io.PrintStream;
@@ -84,7 +100,7 @@ import java.util.SortedSet;
  * <i>must</i> support a parameterless constructor.
  * 
  * <p>
- * <b>Last javadoc Review:</b> Jan de Jongh, TNO, 20180402, r5.1.0.
+ * <b>Last javadoc Review:</b> Jan de Jongh, TNO, 20180404, r5.1.0.
  * 
  * @param <E> The type of {@link SimEvent}s supported.
  * 
