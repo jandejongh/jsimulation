2790bba8310ec51b83dde5754ef7038f23abc77a
jan <jan@rush.jdj>
2018-04-04 19:34:13 +0200

    r5.1.0 release.

--- src/main/java/nl/jdj/jsimulation/r5/DefaultSimEventFactory.java
+++ src/main/java/nl/jdj/jsimulation/r5/DefaultSimEventFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010-2018 Jan de Jongh, TNO.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package nl.jdj.jsimulation.r5;
 
 /** A default {@link SimEventFactory} for {@link SimEvent}s
@@ -5,11 +21,20 @@ package nl.jdj.jsimulation.r5;
  * <p>
  * The factory generates {@link DefaultSimEvent}s.
  * 
+ * <p>
+ * <b>Last javadoc Review:</b> Jan de Jongh, TNO, 20180404, r5.1.0.
+ * 
  */
 public class DefaultSimEventFactory
 implements SimEventFactory<SimEvent>
 {
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SimEventFactory
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
   /** Returns a new {@link DefaultSimEvent}.
    * 
    * @return A new {@link DefaultSimEvent}.
@@ -20,5 +45,11 @@ implements SimEventFactory<SimEvent>
   {
     return new DefaultSimEvent (name, time, null, eventAction);
   }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
 }
