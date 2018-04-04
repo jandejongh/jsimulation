185dc981bee76cd266f3a9f60e455f95e943575a
jan <jan@rush.jdj>
2018-04-04 19:28:16 +0200

    r5.1.0 release.

--- src/main/java/nl/jdj/jsimulation/r5/DefaultSimEventComparator.java
+++ src/main/java/nl/jdj/jsimulation/r5/DefaultSimEventComparator.java
@@ -1,11 +1,31 @@
+/*
+ * Copyright 2010-2018 Jan de Jongh, TNO.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package nl.jdj.jsimulation.r5;
 
 import java.util.Comparator;
 
 /** A default {@link Comparator} on {@link SimEvent}s.
  * 
+ * <p>
  * This comparator extends the partial ordering of {@link SimEvent}s using their time property
  * to a total ordering using, in case of a tie in the event times, the de-conflict field of the event.
+ * 
+ * <p>
+ * <b>Last javadoc Review:</b> Jan de Jongh, TNO, 20180404, r5.1.0.
  * 
  * @param <E> The type of {@link SimEvent}s supported.
  * 
@@ -15,9 +35,16 @@ import java.util.Comparator;
 public class DefaultSimEventComparator<E extends SimEvent> implements Comparator<E>
 {
 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // Comparator
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
   @Override
-  public int compare (E e1, E e2)
+  public int compare (final E e1, final E e2)
   {
+    // XXX What if e1 == null || e2 == null?
     int c = Double.compare (e1.getTime (), e2.getTime ());
     if (c == 0)
       c = Long.compare (e1.getSimEventListDeconflictValue (), e2.getSimEventListDeconflictValue ());
