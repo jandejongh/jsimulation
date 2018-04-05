612db587f8979e2519dba089427fe159371876d2
jan <jan@rush.jdj>
2018-04-05 11:26:15 +0200

    r5.1.0 release: - License headers,

--- src/main/java/nl/jdj/jsimulation/r5/DefaultSimEventComparator.java
+++ src/main/java/nl/jdj/jsimulation/r5/DefaultSimEventComparator.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2010-2018 Jan de Jongh, TNO.
+/* 
+ * Copyright 2010-2018 Jan de Jongh <jfcmdejongh@gmail.com>, TNO.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ * 
  */
 package nl.jdj.jsimulation.r5;
 
